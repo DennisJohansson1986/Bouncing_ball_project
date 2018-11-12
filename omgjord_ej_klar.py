50ad764e4361f3b39a00005cb658722238cfe1bb
dennis johansson <dennisjohansson1986@gmail.com>
2018-11-12 16:03:18 +0100

    lite små ändringar

--- omgjord_ej_klar.py
+++ omgjord_ej_klar.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from collections import defaultdict
 
 import random
 import time
@@ -11,21 +12,26 @@ class Game:
         self.master = master
         self.create_window()
         self.create_objects()
-        self.update_()
+        self.welcome_text = self.create_text(300,200,"press enter to start")
+        self.game_update()
         self.start()
 
 
     def create_objects(self):
         self.paddle = Paddle(self.canvas, "red")  # -||-
         self.ball = Ball(self.canvas, "blue")  # ta bort om detta inte funkar
-        self.bricks = Obstacle(self.canvas) # inte såhär
+        self.lives = Lives(self.canvas,"blue", 3)
+        self.bricks = Obstacle(self.canvas)
 
     def start(self):
         self.master.mainloop()
 
+    def create_text(self, x_pos, y_pos, text,size="40"):
+        font = ('Helvetica', size)
+        return self.canvas.create_text(x_pos, y_pos, text=text, font=font)
 
     def create_window(self):  # bort för v.1
-        self.width = 610
+        self.width = 600
         self.height = 400
         self.master.title("Bouncing Ball Game")
         self.master.resizable(0, 0)
@@ -33,54 +39,77 @@ class Game:
         self.canvas = Canvas(self.master, width=self.width, height=self.height, bd=0, highlightthickness=0)
         self.canvas.pack()
 
+    def update_lives(self):
+
+        if self.ball.get_position()[3] >= self.canvas.winfo_height():
+            self.lives.life -= 1
+            if self.lives.life == 0:
+                self.ball.y = 0
+                self.create_text(300, 200, "Game Over")
+        if self.lives.life == 2:
+            self.canvas.itemconfig(self.lives.id, text="Lives:" + str(self.lives.life), fill="blue", font=('Helvetica',15) )
+        if self.lives.life == 1:
+            self.canvas.itemconfig(self.lives.id ,text="Lives:" + str(self.lives.life), fill="red", font=('Helvetica', 15))
 
 
-    def update_(self):
+    def game_update(self):
 
         while 1:
-            if not self.ball.hit_bottom:
-                if self.ball.throw_ball == True:
+
+            if not self.lives.life == 0:
+                self.paddle.move_paddle()
+                if self.ball.throw_ball:
+                    self.canvas.delete(self.welcome_text)
                     self.ball.move_ball()
-                    self.paddle.move_paddle()
+                    self.ball.update_dir()
                     self.ball_paddle_hit()
+                    self.update_lives()
                 self.master.update_idletasks()
                 self.master.update()
                 time.sleep(0.01)
             else:
+                time.sleep(2)
                 exit()
+
 
     def ball_paddle_hit(self):
         paddle_pos = self.canvas.coords(self.paddle.id)
         self.ball.paddle_hit(paddle_pos)
 
-#   def get_position(self, item):
- #       return self.canvas.coords(item)
+    # def brick_collision(self):
+    #     self.brick_pos = self.canvas.coords(self.bricks.id)
+    #     self.brick_bounce = self.ball.collision_bricks(self.brick_pos)
+    #     if self.brick_bounce == 4:
+    #         self.ball.x = 1
+    #     if self.brick_bounce == 3:                                                denna funkar inte än
+    #         self.ball.x = -1
+    #     if self.brick_bounce == 1:
+    #         self.ball.y = -1
+    #     if self.brick_bounce == 2:
+    #         self.ball.y = 1
 
-#    def move_(self, x, y):
-#        self.canvas.move(self, x, y)
 
- #   def delete(self):
- #       self.canvas.delete(self)
-
-
-
+class Lives:
+    def __init__(self, canvas, color, life):
+        self.canvas = canvas
+        self.life = life
+        self.id = self.canvas.create_text(30, 10, text="Lives:" + str(self.life), fill=color, font=('Helvetica',15))
 
 
 class Paddle:
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = self.canvas.create_rectangle(0, 0, 100, 10,fill=color)
-        self.canvas.move(self.id, 200,300)
+        self.canvas.move(self.id, 250,300)
         self.x_dir = 0
         self.y_dir = 0
-        self.paddle_pos = self.canvas.coords(self.id)
+
 
         self.canvas.bind_all('<KeyPress-Left>', self.move_left)
         self.canvas.bind_all('<KeyPress-Right>', self.move_right)
         self.canvas.bind_all('<KeyPress-Up>', self.move_up)
         self.canvas.bind_all('<KeyPress-Down>', self.move_down)
         self.canvas.bind_all('<KeyPress-space>', self.stop_paddle)
-
 
 
     def move_left(self, evt):
@@ -100,7 +129,7 @@ class Paddle:
         paddle_pos = self.canvas.coords(self.id)
         if paddle_pos[0] <= 0:
             self.x_dir = 0
-        if paddle_pos[2] >= 610:
+        if paddle_pos[2] >= 600:
             self.x_dir = 0
         if paddle_pos[1] <= 0:
             self.y_dir = 0
@@ -115,102 +144,90 @@ class Ball:
         random.shuffle(start)
         self.x = start[0]
         self.y = 1
-        self.hit_bottom = False
-        self.canvas.bind_all('<KeyPress-Return>', self.start_game)
+        self.canvas.bind_all('<KeyPress-Return>', self.throw_ball)
         self.throw_ball = False
+        self.canvas.move(self.id, 285, 265)
 
     def paddle_hit(self, p_pos):
         paddle_pos = p_pos
         ball_pos = self.canvas.coords(self.id)
         if ball_pos[2] >= paddle_pos[0] and ball_pos[0] <= paddle_pos[2]:
             if ball_pos[3] >= paddle_pos[1] and ball_pos[3] <= paddle_pos[3]:
-                self.y = -3
+                self.y = -5
                 self.x = random.randrange(-1,1)
         return False
 
-    def move_ball(self):  # Ball
-        #  testa sen när det funkar att ta bort self.item i metoden
+    def get_position(self):
+        return self.canvas.coords(self.id)
+
+    def move_ball(self):
         self.canvas.move(self.id, self.x, self.y)
+
+    def update_dir(self):  # Ball
+        #  testa sen när det funkar att ta bort self.item i metoden
         ball_pos = self.canvas.coords(self.id)
 
         if ball_pos[1] <= 0:
-            self.y = 1
+            self.y = 2
         if ball_pos[3] >= 400:
-            self.hit_bottom = True
+            self.y = -20
+            self.x = 0
         if ball_pos[0] <= 0:
-            self.x = 1
-        if ball_pos[2] >= 610:
-            self.x = -1
+            self.x = 2
+        if ball_pos[2] >= 600:
+            self.x = -2
 
-    def start_game(self, evt):
+
+    def throw_ball(self, evt):
         self.throw_ball = True
 
-    def collision_bricks(self, bricks):
-        number_collision = 0
-        ball_pos = self.canvas.coords(self.id)
-        for brick in bricks:  # iterate through list
-            if ball_pos[3] == brick[1] and brick[0] <= ball_pos[0] <= brick[2]:  # for a hit from over
-                self.y = -1
-            if ball_pos[1] == brick[3] and brick[0] <= ball_pos[0] <= brick[2]:  # for a hit from under
-                self.y = 1
-            if ball_pos[2] == brick[0] and brick[1] <= ball_pos[1] <= brick[3]:  # for a hit from right side
-                self.x = -1
-            if ball_pos[0] == brick[2] and brick[1] <= ball_pos[1] <= brick[3]:  # for a hit from left side
-                self.x = 1
-
-        return number_collision
-#här låg tidigare collision
-
-
-
+    # def collision_bricks(self, bricks):
+    #     brick_hit = 0
+    #     ball_pos = self.canvas.coords(self.id)
+    #     for brick in bricks:  # iterate through list
+    #         if ball_pos[3] == brick[1] and brick[0] <= ball_pos[0] <= brick[2]:  # for a hit from over
+    #             brick_hit += 1
+    #             self.hit = self.canvas.find_closest(brick[0], brick[1], halo=5)
+    #         if ball_pos[1] == brick[3] and brick[0] <= ball_pos[0] <= brick[2]:  # for a hit from under               funkar inte än
+    #             brick_hit += 2
+    #             self.hit = self.canvas.find_closest(brick[0], brick[1], halo=5)
+    #         if ball_pos[2] == brick[0] and brick[1] <= ball_pos[1] <= brick[3]:  # for a hit from right side
+    #             brick_hit += 3
+    #             self.hit = self.canvas.find_closest(brick[0], brick[1], halo=5)
+    #         if ball_pos[0] == brick[2] and brick[1] <= ball_pos[1] <= brick[3]:  # for a hit from left side
+    #             brick_hit += 4
+    #             self.hit = self.canvas.find_closest(brick[0], brick[1], halo=5)
+    #     return brick_hit
 
 class Obstacle:
     def __init__(self, canvas):
         self.canvas = canvas
-        self.id = self.bricks
+        self.id = self.level(self.canvas)
 
-    def create_level(self):
+    def level(self, canvas):
+        self.canvas = canvas
+        self.bricks = []
         level = 1
         row = 0
-        self.bricks = []
+
+
         try:
 
             for line in open(str(level) + ".txt", "r"):
-                row = row + 1
+                row = row +1
                 data = line.split(";")
                 for i in range(12):
                     if data[i] == ".":
-                        pass #kan man ha continue istället?
+                        pass
                     else:
-                        brick = (0 + (50 * i), 20 + (row * 20), 50 + (50 * i), 40 + (row * 20), data[i])
-                        self.bricks.append(brick)
+                        self.brick_cord = (0 + (50 * i), 20 + (row * 20), 50 + (50 * i), 40 + (row * 20), data[i])
+                        self.bricks.append(self.canvas.create_rectangle(self.brick_cord[0], self.brick_cord[1], self.brick_cord[2], self.brick_cord[3], fill=self.brick_cord[4], width=2, outline="#ffffff"))
+
         except IOError:
             pass
 
-            for bricks in self.bricks:
-                pos1 = bricks[0]
-                pos2 = bricks[1]
-                pos3 = bricks[2]
-                pos4 = bricks[3]
-                color = bricks[4]
-                self.canvas.create_rectangle(pos1, pos2, pos3, pos4, fill=color)
-            return self.bricks
+        return self.bricks
 
 #starts the game
 root = Tk()
 start = Game(root)
-
-"""
-    def add_brick(self, x, y, hits):
-        brick = Brick(self.canvas, x, y, hits)
-        self.items[brick.item] = brick
-"""
-"""
-        for l in self.bricks:
-            pos1 = l[0]
-            pos2 = l[1]
-            pos3 = l[2]
-            pos4 = l[3]
-            color = l[4]
-            self.canvas.create_rectangle(pos1, pos2, pos3, pos4, fill=color)
-"""
